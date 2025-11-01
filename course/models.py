79913c973c811aa9ffbac4230c2fcaa250d67d0c
Shobanbabu03 <shobanbaburishi03@gmail.com>
2025-11-01 18:21:42 +0530

    Project Loaded

---
+++ course/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    trainer = models.ForeignKey
+
